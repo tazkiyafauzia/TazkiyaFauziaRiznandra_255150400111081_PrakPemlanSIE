6be927253bb24ee3f23ffef24fd862384d06f98f
tazkiyafauzia <tazkiyafauziaa@gmail.com>
2026-03-25 07:19:15 +0700

    Implement SchoolApp to manage student names and scores

---
+++ Modul 2/Enkapsulasi/SchoolApp.java
@@ -0,0 +1,17 @@
+public class SchoolApp {
+    public static void main(String[] args) {
+        Student miki = new Student("Miki");
+        Student mini = new Student("Mini");
+
+        System.out.println("Grade " + miki.getNama() + " " + miki.getSkor());
+        System.out.println("Grade " + mini.getNama() + " " + mini.getSkor());
+
+        mini.ubahNama("Mono");
+
+        System.out.println("Grade " + miki.getNama() + " " + miki.getSkor());
+        System.out.println("Grade " + mini.getNama() + " " + mini.getSkor());
+
+        Student.setNama(miki, "Kiki");
+        System.out.println("Grade " + miki.getNama() + " " + miki.getSkor());
+    }
+}
