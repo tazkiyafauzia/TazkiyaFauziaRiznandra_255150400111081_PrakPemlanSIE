9ac2bbe859f7abea7d273e3c9979f3d1871343e2
tazkiyafauzia <tazkiyafauziaa@gmail.com>
2026-03-25 07:11:17 +0700

    Add Lingkaran class with static and non-static modifier

---
+++ Modul 2/Member Static dan Non-static/Lingkaran.java
@@ -0,0 +1,16 @@
+public class Lingkaran {
+    public static float PI = 3.14f;
+    public float r;
+
+    public Lingkaran(float r) { 
+        this.r = r;
+    }
+
+    public float luas() {
+        return Lingkaran.PI * r * r;
+    }
+
+    public float keliling() {
+        return 2 * Lingkaran.PI * r;
+    }
+}
