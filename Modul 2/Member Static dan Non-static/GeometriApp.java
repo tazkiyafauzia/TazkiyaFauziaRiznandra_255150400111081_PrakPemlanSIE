eb9bcd0c3b5a73fa4307b2de530a9282231ad14d
tazkiyafauzia <tazkiyafauziaa@gmail.com>
2026-03-25 07:20:23 +0700

    Add GeometriApp to calculate circle area and perimeter

---
+++ Modul 2/Member Static dan Non-static/GeometriApp.java
@@ -0,0 +1,18 @@
+public class GeometriApp {
+    public static void main(String[] args) {
+        Lingkaran lingKecil = new Lingkaran(2.0f);
+        Lingkaran lingBesar = new Lingkaran(11.0f);
+
+        float lK = lingKecil.luas();
+        float lB = lingBesar.luas();
+
+        System.out.println("Luas Lingkaran Kecil: " + lK);
+        System.out.println("Luas Lingkaran Besar: " + lB);
+
+        float kK = lingKecil.keliling();
+        float kB = lingBesar.keliling();
+
+        System.out.println("Keliling Lingkaran Kecil: " + kK);
+        System.out.println("Keliling Lingkaran Besar: " + kB);
+    }
+}
